b68d3fb2cd42c16f02cd0a30c420484b96930103
Tom Pearson <tom.g.pearson@gmail.com>
2023-10-10 13:19:21 +0100

    redo styling

--- src/components/bookstack.svelte
+++ src/components/bookstack.svelte
@@ -1,15 +1,18 @@
 <script>
   import Book from './Book.svelte';
 
-  import { scaleLinear, max, scaleOrdinal,scaleBand, range} from 'd3';
+  import { scaleLinear, scaleBand} from 'd3';
 
   export let books = [];
   export let title = "books";
-  export let height = 400;
+  export let height = 350;
   export let width = 700;
-  export let margin = { top:50, left:20, bottom:10, right:20 };
-  export let jitter = 0.1;
+  export let margin = { top:50, left:0, bottom:0, right:0 };
+  export let jitter = 0.18;
+  export let minPages = 100;
   export let splitOnKey = book => "books";
+
+  console.log(jitter);
 
   let meanAspectRatio = 3000; // if 1 page is 1 pixel thick the length of the books spine should be this number of px
   let meanSpineLength = meanAspectRatio;
@@ -23,6 +26,7 @@
   let pageScale = scaleLinear();
   let stackScale = scaleBand();
 
+  // split into stacks
   $:{
     books.forEach(book=>{
       if(!stacks[splitOnKey(book)]){
@@ -33,26 +37,20 @@
         };
       }
 
-      stacks[splitOnKey(book)].pages += Number(book.pages);
+      stacks[splitOnKey(book)].pages += Math.max(Number(book.pages), minPages);
       if(biggestStack < stacks[splitOnKey(book)].pages){
         biggestStack = stacks[splitOnKey(book)].pages;
       }
       stacks[splitOnKey(book)].bookCount ++;
       stacks[splitOnKey(book)].books.push(book);
     });
-  
-    // how many stacks are there...
     stackCount = Object.keys(stacks).length;
-    // console.log(Object.values(stacks).map(d=>d.bookCount));
-    // console.log('>>', stackCount, biggestStack);
-    // console.log(stacks);
   }
 
 
 
   // work out the scales
   $:{
-    
     pageScale
       .domain([0, biggestStack])
       .range([0, plotHeight]);
@@ -66,7 +64,6 @@
     meanSpineLength = pageScale(1) * meanAspectRatio;
     console.log('S', meanSpineLength);
     Object.entries(stacks).forEach(layoutStack);
-
   }
 
   function layoutStack([key, stack], i){    
@@ -76,11 +73,11 @@
       if(!book.pages || book.pages==''){
         book.ignore = true;
       }
-      let jitterwidth = stackScale.bandwidth()/20
-      let jitter = Math.random()*(jitterwidth) - jitterwidth/2;
-      book.x = 0 + jitter;
-      book.width = Math.min(stackScale.bandwidth(), meanSpineLength + jitter)
-      book.height = pageScale(Number(book.pages));
+      let jitterwidth = stackScale.bandwidth() * jitter;
+      let bookJitter = Math.random()*(jitterwidth) - jitterwidth/2;
+      book.x = 0 + bookJitter;
+      book.width = Math.min(stackScale.bandwidth(), meanSpineLength + bookJitter)
+      book.height = pageScale(Math.max(Number(book.pages),minPages));
       acc+= book.height;
       book.y = plotHeight-acc
       return book;
@@ -89,29 +86,27 @@
 
 </script>
 {title}
-chart...
-<div>
-<svg viewBox="0 0 {width} {height}" height={height} width={width}>
-  <g class="plot" transform="translate({margin.left}, {margin.top})">
-    {#each Object.entries(stacks) as entry, i}
-    <g class="stack" transform="translate({i*plotWidth/stackCount},0)">
-      <text dy="-5">{entry[1].key}</text>
-      {#each entry[1].books as book}
-        {#if !book.ignore}
-        <g transform="translate({book.x},{book.y})">
-          <Book width={book.width} height={book.height} title={book.title} author={" "}></Book>
-        </g>
-        {/if}
+<div class="chart">
+  <svg viewBox="0 0 {width} {height}">
+    <g class="plot" transform="translate({margin.left}, {margin.top})">
+      {#each Object.entries(stacks) as entry, i}
+      <g class="stack" transform="translate({i*plotWidth/stackCount},0)">
+        <text dy="-5">{entry[1].key}</text>
+        {#each entry[1].books as book}
+          {#if !book.ignore}
+          <g transform="translate({book.x},{book.y})">
+            <Book width={book.width} height={book.height} title={book.title} author={" "}></Book>
+          </g>
+          {/if}
+        {/each}
+      </g>
       {/each}
     </g>
-    {/each}
-  </g>
-</svg>
+  </svg>
 </div>
 <style>
-  .book{
-    fill:black;
-    stroke:white;
+  .chart{
+    width:100%;
   }
   .stack{
     font-family: sans-serif;
