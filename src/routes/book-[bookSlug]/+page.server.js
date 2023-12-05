cd6a3dd4a223fdff51251e0a72402fc7d70b75aa
Tom P <tom.g.pearson@gmail.com>
2023-12-05 11:16:52 +0000

    fix http errors for non eiztent pages

--- src/routes/book-[bookSlug]/+page.server.js
+++ src/routes/book-[bookSlug]/+page.server.js
@@ -3,7 +3,8 @@ import * as fs from 'fs';
 export async function load({params}) {
   const index = JSON.parse(fs.readFileSync('static/data/readIndex.json','utf-8'));
   const pages = index[params.bookSlug];
-  const content = []
+  console.log('P',pages);
+  const content = [];
   pages.forEach(p => {
     const md = fs.readFileSync(`static/markdown/${p}.md`,'utf-8');
     const parts = md.split('---');
