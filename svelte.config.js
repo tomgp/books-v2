5451d1762d3a9d59f1d09e71dca3ce9ea409fc5f
Tom P <tom.g.pearson@gmail.com>
2024-01-16 13:29:37 +0000

    cloud flare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ handleHttpError: 'warn' }),
+		adapter: adapter(),
 		prerender:{
 			handleHttpError:(msg)=>{
-				console.log('MMM',msg);
+				console.log('Prerender error',msg);
 			}
 		}
 	}
