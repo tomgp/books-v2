c136586d346e26cc56b1abcc3e5df1a91f3f9f45
Tom P <tom.g.pearson@gmail.com>
2024-01-22 10:04:11 +0000

    roadwork notes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,10 @@
 <h1>About</h1>
 <p>As of January 2024 this website is a work in progress</p>
-<p>I'm publishing it now because if I don't I'll fiddle around with it forever & to live with it will help me understand where I want to take it</p>
-<p>My old books page can be found <a href="https://www.toffeemilkshake.co.uk/books">here</a></p>
+<p>My old books page can be found <a href="https://www.toffeemilkshake.co.uk/books">here</a> but I'll not be updating it</p>
 <h2>Aims</h2>
 <ul class="bulleted">
   <li>More colorful than the old page</li>
-  <li>Include photos of books covers</li>
+  <li>Include photos of book covers</li>
   <li>Allow for notes on books</li>
   <li>... and links to related webpages etc.</li>
 </ul>
