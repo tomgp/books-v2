8273ca719e2ff70d9acee26fd17470bb9e8d80f9
Tom P <tom.g.pearson@gmail.com>
2024-01-23 11:27:17 +0000

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,8 @@
 <h1>About</h1>
 <p>As of January 2024 this website is a work in progress</p>
-<p>Notes about books are not well thouyght out opinions but just whatever I've put in my notes app whilst reading with asome light editing to join them together.</p>
+<p>Notes about books are not well thought out opinions but just whatever I've put in my notes app whilst reading with some light editing to join them together. I'm bad at spelling and make lots of typos, sorry about that.</p>
 <p>My old books page can be found <a href="https://www.toffeemilkshake.co.uk/books">here</a> but I'll not be updating it.</p>
-<h2>Aims</h2>
+<h2>Initial aims</h2>
 <ul class="bulleted">
   <li>More colorful than the old page</li>
   <li>Include photos of book covers</li>
@@ -17,10 +17,13 @@
   <li><a href="https://maggieappleton.com/library">Maggie Appleton</a></li>
   <li><a href="https://macwright.com/reading/">Tom MacWright</a> (so many Toms)</li>
 </ul>
-<h2>Todo... </h2>
+<h2>Colophon</h2>
+<p>This site was built using <a href="https://kit.svelte.dev/">Sveltekit</a> and a bit of <a href="https://d3js.org/">D3</a> deployed as a collection of static HTML/CSS/JS files (as god intended) on Cloudflare pages, it uses the free <a href="https://www.productiontype.com/news/new_fonts_from_production_type_newsreader">Newsreader font</a>, sets no cookies and collects no data.</p>
+<h2>Todo list</h2>
 <ul class="bulleted">
   <li>Booklist should be split into years and months ✔️</li>
   <li>Make book list filterable</li>
+  <li>Back and next links on book pages</li>
   <li>Add notes and reviews that I've written elsewhere if I can find them</li>
   <li>Add external links for books and authors</li>
   <li>More photographs</li>
