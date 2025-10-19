84bf4c48c64f8b57538e149e34dc980ed444a7c4
Tom Pearson <tom.pearson@carbonbrief.org>
2025-10-19 08:13:29 +0100

    the double

--- src/routes/book-[bookSlug]/+page.server.js
+++ src/routes/book-[bookSlug]/+page.server.js
@@ -1,22 +1,23 @@
 import * as fs from 'fs';
-import {csvParse} from 'd3';
+import { csvParse } from 'd3';
 import { getMD } from '$lib/get-md.server.js';
 
 export async function load({ params }) {
 	const index = JSON.parse(fs.readFileSync('static/data/readIndex.json', 'utf-8'));
-	const links = csvParse(fs.readFileSync('static/data/links.csv', 'utf-8'))
-		.filter(link=>link.bookslug == params.bookSlug);
+	const links = csvParse(fs.readFileSync('static/data/links.csv', 'utf-8')).filter(
+		(link) => link.bookslug == params.bookSlug
+	);
 	const reads = index[params.bookSlug];
 	const content = [];
 
-	if(reads){
+	if (reads) {
 		reads.forEach((datedBookSlug) => {
 			let file = `static/markdown/${datedBookSlug}.md`;
 			let readData = getMD(file);
 			content.push(readData);
 		});
 	}
-	
+
 	return {
 		slug: params.bookSlug,
 		content: content,
